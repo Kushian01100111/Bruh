3fe2ab163124e86be4ad659e077d6304aa946cc9
Kushian01100111 <comaspedro6@gmail.com>
2022-12-15 14:07:23 -0300

    1 de 3

--- 12.12.js
+++ 12.12.js
@@ -13,3 +13,20 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
+// array only nums => any length 
+// return true if the a element is repeted twice false other wise
+
+function repetedNum(arr){
+     // loop thougth array  
+    for(let i = 0; i < arr.length; i++){
+        let temp = arr[i]
+        // if current index of the element is the last index on which this element appered
+        if(arr.lastIndexOf(temp) !== i) return true
+    }
+    // esle return false
+    return false
+}
+
+console.log(repetedNum([1,2,3,1]),true)
+console.log(repetedNum([1,2,3,4]),false)
+console.log(repetedNum([1,1,1,3,3,4,3,2,4,2]),true)
