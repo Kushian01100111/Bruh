2b95f8797d417099a3aeb3b386f4607753225940
Kushian01100111 <comaspedro6@gmail.com>
2022-12-26 20:45:31 -0300

    Vainas2

--- 15.12.js
+++ 15.12.js
@@ -5,3 +5,26 @@
 //   assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
 //  });
 // });
+
+// array || given sum
+// pairs of value of the array that add up to the given sum 
+
+function twoSum(arr,givenNum){
+    // 2 variables => With the pair of Nums || => with nums taken into account 
+    let pairs = [],
+        accountNums = {};
+    // for loop => 1 givenNum - current  2 check hash map=> push the [result,current]
+    for(const current of arr){
+        let result = givenNum - current;
+        if(accountNums[result]){
+            pairs.push([current,result]);
+        }else{
+            // 3 new entrie with the current 
+            accountNums[current] = 1;
+        }
+    }
+    // return array contaning the pairs
+    return pairs
+}
+
+console.log(twoSum([1,2,2,3,4], 4),"[[2,1],[3,1]]")
